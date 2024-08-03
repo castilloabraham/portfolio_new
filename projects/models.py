0580cc75f7d849002257ecb063b7ec03a1f7b14b
castilloabraham <adcdabrahamcastillo@gmail.com>
2024-08-03 16:50:23 -0400

    Se creo el modelo que se usara para los proyectos y se importo pillow

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, verbose_name='titulo')
+    tecnology = models.CharField(max_length=100, verbose_name='tecnologia')
+    description = models.TextField(verbose_name="Descripción", null=True)
+    main_image = models.ImageField(upload_to='imagenes/',verbose_name="imagen",null=True)
+    publish = models.BooleanField(default=False)
+    
+    #este constructor te retorna la informacion en la table del super admin
+    def __str__(self):
+        return self.title
+    

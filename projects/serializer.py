94fd87aa5e08cb821478a4be78a230d14afde1b9
castilloabraham <adcdabrahamcastillo@gmail.com>
2024-08-04 19:42:35 -0400

    Creacion del Serializer para conversion de datos en queryset

---
+++ projects/serializer.py
@@ -0,0 +1,11 @@
+#este archivo hara la conversion de mis campos en queryset
+
+from rest_framework import serializers
+from .models import Project
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        #campos que desea convertir
+        #fields = ('nombre del campo')
+        fields = '__all__'

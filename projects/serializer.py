51621eb0a6eea104c1240ba153ea72ecccaf745c
castilloabraham <adcdabrahamcastillo@gmail.com>
2024-08-04 20:53:30 -0400

    cambio del nombre de la class en serializer (error de tipeo)

--- projects/serializer.py
+++ projects/serializer.py
@@ -3,7 +3,7 @@
 from rest_framework import serializers
 from .models import Project
 
-class TaskSerializer(serializers.ModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         #campos que desea convertir

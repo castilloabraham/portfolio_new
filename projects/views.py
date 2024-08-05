ce18686d8f654e12700cf7652ff4bf658ee2f7ea
castilloabraham <adcdabrahamcastillo@gmail.com>
2024-08-04 20:44:37 -0400

    creacion de la vista para el crud

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ProjectSerializer #Ya nos dice que campos vamos a compartir y recibir
+from .models import Project
 
 # Create your views here.
+
+
+# con esta clase generamos el crud de forma automatica
+class ProjectView(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all() #Se esta trayendo todos los proyectos
+    
